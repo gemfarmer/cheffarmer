ddecafc88a4010fdfabc42d154b58bf574333024
Brian Hedberg <bshslowfood@gmail.com>
2015-03-12 13:04:50 -0500

    roles can change between farmer and chef

--- client/app/account/signup/signup.controller.js
+++ client/app/account/signup/signup.controller.js
@@ -12,7 +12,8 @@ angular.module('cheffarmerApp')
         Auth.createUser({
           name: $scope.user.name,
           email: $scope.user.email,
-          password: $scope.user.password
+          password: $scope.user.password,
+          role: $scope.user.role
         })
         .then( function() {
           // Account created, redirect to home
